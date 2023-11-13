3a6184288cb7ecf99075779fe5b348b439068bf6
Benedicta Onyebuchi <benedictauche.onyebuchi@gmail.com>
2023-11-13 11:37:41 +0100

    it
    testing

--- src/Pages/Auth/Signup.jsx
+++ src/Pages/Auth/Signup.jsx
@@ -14,8 +14,8 @@ function InputField({
   error,
 }) {
   return (
-    <div className="inline-flex flex-col sm:w-full lg:w-1/2">
-      <label htmlFor={name} className="text-xl font-medium">
+    <div className="mb-4 sm:w-full lg:w-1/2">
+      <label htmlFor={name} className="block text-lg font-medium">
         {label}
       </label>
       <input
@@ -25,7 +25,7 @@ function InputField({
         value={value}
         placeholder={placeholder}
         onChange={onChange}
-        className="w-[350px] px-6 h-[50px] rounded-[10px] border-2 border-neutral-400 placeholder:text-stone-400 placeholder:text-lg font-medium"
+        className="w-full px-4 py-2 rounded-md border-2 border-gray-300 placeholder-gray-500 text-lg font-medium"
       />
       {error && <p className="text-red-500 text-sm">{error}</p>}
     </div>
@@ -34,8 +34,8 @@ function InputField({
 
 function SelectField({ label, name, id, value, onChange, error }) {
   return (
-    <div className="inline-flex flex-col">
-      <label htmlFor={name} className="text-xl font-medium">
+    <div className="mb-4">
+      <label htmlFor={name} className="block text-lg font-medium">
         {label}
       </label>
       <select
@@ -43,7 +43,7 @@ function SelectField({ label, name, id, value, onChange, error }) {
         id={id}
         value={value}
         onChange={onChange}
-        className="w-[350px] px-6 h-[50px] rounded-[10px] border-2 border-neutral-400 placeholder:text-stone-400 placeholder:text-lg font-medium"
+        className="w-full px-4 py-2 rounded-md border-2 border-gray-300 placeholder-gray-500 text-lg font-medium"
       >
         <option value="A+">A+</option>
         <option value="A-">A-</option>
@@ -57,7 +57,7 @@ function SelectField({ label, name, id, value, onChange, error }) {
   );
 }
 
-const Signup = ({ onLogin }) => {
+const Signup = () => {
   const [form, setForm] = useState({
     name: "",
     email: "",
@@ -85,8 +85,11 @@ const Signup = ({ onLogin }) => {
     }));
     setErrors({ ...errors, [name]: undefined });
   };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setIsSubmitting(true);
+
     try {
       const response = await axios.post(
         `${baseUrl}/blood-savers/signup`,
@@ -97,6 +100,7 @@ const Signup = ({ onLogin }) => {
           },
         }
       );
+
       if (response.status === 201) {
         toast.success("Successfully created an account", {
           position: toast.POSITION.TOP_RIGHT,
@@ -106,7 +110,6 @@ const Signup = ({ onLogin }) => {
         console.error("Signup failed");
         toast.error("Error creating account");
       }
-
     } catch (error) {
       if (error.response) {
         setIsSubmitting(false);
@@ -122,147 +125,149 @@ const Signup = ({ onLogin }) => {
       } else {
         toast.error(error.response?.data?.message);
       }
-      setIsSubmitting(false);
     }
+    setIsSubmitting(false);
   };
 
-  console.log(form);
-
   return (
-    <>
-      <div className="flex flex-col justify-center items-center align-middle mt-10">
-        <div>
-          <h2 className="text-[40px] font-bold text-center">Register</h2>
-          <p className="text-center text-stone-400 text-lg font-medium py-6">
-            Join BloodSavers help save lives
-          </p>
-          <form
-            action=""
-            className="flex flex-col gap-8 sm:items-center"
-            onSubmit={handleSubmit}
-          >
-            <div className="flex gap-6 sm:flex-col sm:gap-4 lg:flex-row lg:gap-8">
-              <InputField
-                label="Full Name"
-                type="text"
-                name="name"
-                id="name"
-                placeholder="Enter full name"
-                error={errors?.name}
-                onChange={handleChange}
-              />
-              <InputField
-                label="Email"
-                type="email"
-                name="email"
-                id="email"
-                placeholder="johndoe@example.com"
-                onChange={handleChange}
-              />
-            </div>
+    <div className="flex flex-col justify-center items-center align-middle mt-10">
+      <div className="w-full max-w-md">
+        <h2 className="text-3xl font-bold text-center mb-4">Register</h2>
+        <p className="text-center text-gray-600 text-lg font-medium mb-6">
+          Join BloodSavers and help save lives
+        </p>
+        <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+          <div className="flex flex-col md:flex-row md:gap-4 lg:flex-row lg:gap-8">
+            <InputField
+              label="Full Name"
+              type="text"
+              name="name"
+              id="name"
+              placeholder="Enter full name"
+              error={errors?.name}
+              value={form.name}
+              onChange={handleChange}
+            />
+            <InputField
+              label="Email"
+              type="email"
+              name="email"
+              id="email"
+              placeholder="johndoe@example.com"
+              error={errors?.email}
+              value={form.email}
+              onChange={handleChange}
+            />
+          </div>
 
-            <div className="flex gap-6 sm:flex-col sm:gap-4 lg:flex-row lg:gap-8">
-              <InputField
-                label="Country"
-                type="text"
-                name="country"
-                id="country"
-                placeholder="USA"
-                error={errors?.country}
-                onChange={handleChange}
-              />
-              <SelectField
-                label="Blood Group"
-                name="bloodGroup"
-                id="bloodGroup"
-                error={errors?.bloodGroup}
-                onChange={handleChange}
-              />
-            </div>
+          <div className="flex flex-col md:flex-row md:gap-4 lg:flex-row lg:gap-8">
+            <InputField
+              label="Country"
+              type="text"
+              name="country"
+              id="country"
+              placeholder="USA"
+              error={errors?.country}
+              value={form.country}
+              onChange={handleChange}
+            />
+            <SelectField
+              label="Blood Group"
+              name="bloodGroup"
+              id="bloodGroup"
+              error={errors?.bloodGroup}
+              value={form.bloodGroup}
+              onChange={handleChange}
+            />
+          </div>
 
-            <div className="flex gap-6 sm:flex-col sm:gap-4 lg:flex-row lg:gap-8">
-              <InputField
-                label="State/Province"
-                type="text"
-                name="state"
-                id="state"
-                placeholder="California"
-                error={errors?.state}
-                onChange={handleChange}
-              />
-              <InputField
-                label="City"
-                type="text"
-                name="city"
-                id="city"
-                placeholder="Los Angeles"
-                error={errors?.city}
-                onChange={handleChange}
-              />
-            </div>
+          <div className="flex flex-col md:flex-row md:gap-4 lg:flex-row lg:gap-8">
+            <InputField
+              label="State/Province"
+              type="text"
+              name="state"
+              id="state"
+              placeholder="California"
+              error={errors?.state}
+              value={form.state}
+              onChange={handleChange}
+            />
+            <InputField
+              label="City"
+              type="text"
+              name="city"
+              id="city"
+              placeholder="Los Angeles"
+              error={errors?.city}
+              value={form.city}
+              onChange={handleChange}
+            />
+          </div>
 
-            <div className="flex gap-6 sm:flex-col sm:gap-4 lg:flex-row lg:gap-8">
-              <InputField
-                label="Phone Number"
-                type="tel"
-                name="phone"
-                id="phone"
-                placeholder="+1 234 5678 90"
-                error={errors?.phone}
-                onChange={handleChange}
-              />
-              <InputField
-                label="Password"
-                type="password"
-                name="password"
-                id="password"
-                error={errors?.password}
-                onChange={handleChange}
-              />
-            </div>
+          <div className="flex flex-col md:flex-row md:gap-4 lg:flex-row lg:gap-8">
+            <InputField
+              label="Phone Number"
+              type="tel"
+              name="phone"
+              id="phone"
+              placeholder="+1 234 5678 90"
+              error={errors?.phone}
+              value={form.phone}
+              onChange={handleChange}
+            />
+            <InputField
+              label="Password"
+              type="password"
+              name="password"
+              id="password"
+              error={errors?.password}
+              value={form.password}
+              onChange={handleChange}
+            />
+          </div>
 
-            <div>
-              <label htmlFor="profile">
-                <div className="inline-flex gap-2 items-center justify-center cursor-pointer">
-                  <span className="text-[30px] text-red-800">📷</span>
-                  <p className="text-red-800 text-lg font-medium">
-                    Upload Profile Picture
-                  </p>
-                </div>
-              </label>
-              <input
-                type="file"
-                name="profile"
-                id="profile"
-                accept="image/*"
-                className="invisible"
-                onChange={handleChange}
-                disabled={isSubmitting}
-              />
-              {errors?.profile && (
-                <p className="text-red-500 text-sm">{errors?.profile}</p>
-              )}
-            </div>
+          <div>
+            <label htmlFor="profile">
+              <div className="flex items-center gap-2 cursor-pointer">
+                <span className="text-2xl text-red-800">📷</span>
+                <p className="text-red-800 text-lg font-medium">
+                  Upload Profile Picture
+                </p>
+              </div>
+            </label>
+            <input
+              type="file"
+              name="profile"
+              id="profile"
+              accept="image/*"
+              className="invisible"
+              onChange={handleChange}
+              disabled={isSubmitting}
+            />
+            {errors?.profile && (
+              <p className="text-red-500 text-sm">{errors?.profile}</p>
+            )}
+          </div>
 
-            <div className="inline-flex items-center justify-center">
-              <button
-                type="submit"
-                className="bg-red-800 rounded-[15px] h-[2.7em] w-[200px] mx-auto text-white text-lg font-medium"
-              >
-                Register
-              </button>
-            </div>
-          </form>
+          <div>
+            <button
+              type="submit"
+              className="w-full bg-red-800 rounded-md py-2 text-white text-lg font-medium"
+              disabled={isSubmitting}
+            >
+              {isSubmitting ? "Registering..." : "Register"}
+            </button>
+          </div>
+        </form>
 
-          <p className="py-3">
-            Already have an account?{" "}
-            <Link to="/login">
-              <span className="text-blue-400">Login</span>
-            </Link>
-          </p>
-        </div>
+        <p className="text-center py-3">
+          Already have an account?{" "}
+          <Link to="/login" className="text-blue-400">
+            Login
+          </Link>
+        </p>
       </div>
-    </>
+    </div>
   );
 };
 
